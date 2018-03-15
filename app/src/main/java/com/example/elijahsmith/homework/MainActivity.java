f1e6a361e4efc6ff02bf10b9f16b639625fed907
elijahsmith <elijahsmith268@gmail.com>
2018-03-15 08:39:12 -0400

    Added a welcome message

--- app/src/main/java/com/example/elijahsmith/homework/MainActivity.java
+++ app/src/main/java/com/example/elijahsmith/homework/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.elijahsmith.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.start_button);
+
+        startButton = findViewById(R.id.start_button);
+
+        startListener();
+    }
+
+    private void startListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMenu();
+
+            }
+        });
+    }
+
+    private void startMenu() {
+        Intent startup = new Intent(this, HelloActivity.class);
+        startActivity(startup);
+        finish();
+    }
+
+    public void hello_activity(View view) {
     }
 }
